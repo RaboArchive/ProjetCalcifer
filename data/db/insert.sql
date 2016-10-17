188de1383d83f387a906c37ca4aea0e12af5ca6a
Chokoneko <celiac26@live.fr>
2016-10-17 10:57:58 +0200

    Add files via upload

---
+++ data/db/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('La Communauté de l\'Anneau', 'J.R.R Tolkien');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('Les Deux Tours', 'J.R.R Tolkien');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('Le Retour du roi', 'J.R.R Tolkien');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('D\'un monde à l\'autre', 'Pierre Bottero');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('Les frontières de glace', 'Pierre Bottero');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('L\'île du destin', 'Pierre Bottero');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('Le Souffle de la hyène', 'Pierre Bottero');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('Le Maître des tempêtes', 'Pierre Bottero');
+INSERT INTO LIVRE (NOM,AUTEUR) VALUES ('La Huitième Porte', 'Pierre Bottero');
+
+
