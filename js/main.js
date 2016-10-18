68833608a229672996011da1a6f1993c054e75a7
DUBS Victor <dubsvi@hostname.iut2.upmf-grenoble.fr>
2016-10-18 11:11:12 +0200

    recherche

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-$( document ).ready(function() {
-    $("#content").load("./pages/accueil.html");
-});
-
 function launch(){
-  var query = $("#query").val();
+  var query = $("#srchFld").val();
+  console.log(query);
   var data ='data=' + query;
 
   $.ajax({	type: "POST",
@@ -21,17 +18,41 @@ function launch(){
 }
 
 function displayResultContent(data){
-  $("#result").empty();
+  $("#content").empty();
+  var toPrint = '<h4> Résultats de la recherche </h4> <ul class="thumbnails">';
   for (var i=0; i < data.livres.length; i++) {
-      var toPrint = "<div onclick=wantMore("+data.livres[i].idlivre+")> Nom :" + data.livres[i].nom + " | Auteur : " + data.livres[i].auteur;
+	toPrint += '<li class="span3">';
+  //    toPrint += "<div onclick=wantMore("+data.livres[i].idlivre+")> Nom :" + data.livres[i].nom + " | Auteur : " + data.livres[i].auteur;
+
+	toPrint += '<div class="thumbnail">';
+	toPrint += "<a href=\"product.html?id="+data.livres[i].idlivre+"\">";
+	//toPrint += "<img src=\""+data.livres[i].url+"\" alt=\"\"/></a>";
+	toPrint += "<img src=\"jambe.html\" alt=\"\"/></a>";
+	toPrint += '<div class="caption">';
+	toPrint += "<h5>"+data.livres[i].nom+"</h5>";
+	toPrint += "<p>"+data.livres[i].resume+"</p>";
+
+	toPrint += '<h4 style="text-align:center"><a class="btn" href="product.html?id='+data.livres[i].idlivre+'">';
+	toPrint += '<i class="icon-zoom-in"></i></a> <a class="btn" href="#">Ajouter <i class="icon-shopping-cart"></i></a>';
+	if(data.livres[i].val != null){
+	toPrint += '<a class="btn btn-primary" href="#">'+data.livres[i].val+'</a></h4>';
+	} else {
+	toPrint +='</h4>';
+	}
+
+	toPrint += "</div></div></li>";
+	
+/*
+
       if(data.livres[i].etat != null){
         toPrint += " | Etat : " + data.livres[i].etat;
       }
       if(data.livres[i].val != null){
         toPrint += " | Valeur : " + data.livres[i].val;
       }
-      $("#result").append(toPrint+"</div><br>");
+*/
 	}
+	$("#content").append(toPrint);
 }
 
 function wantMore(id){
