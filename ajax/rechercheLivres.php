1cf3f1bf3391f52699ab40be9c40e61fe9aaad4f
Bypus <stockerj@hostname.iut2.upmf-grenoble.fr>
2016-10-19 16:59:18 +0200

    dgsdfg

--- ajax/rechercheLivres.php
+++ ajax/rechercheLivres.php
@@ -6,13 +6,14 @@ $result["status"] = "success";    //Initialisation du premier élément avec "su
 $data = $_POST["data"];
 if(isset($data)){
   //$data = str_replace(" ", "_", $data);
-  $requeteLivreNotice = "SELECT distinct titre,auteur,image FROM livre L, LIVREDEPOSE LD WHERE L.ISBN=LD.ISBN AND (L.titre LIKE \"%$data%\" OR L.auteur LIKE \"%$data%\")";
-  $requeteLivreDepose = "SELECT distinct titre,auteur, image FROM livre L, LIVREDEPOSE LD WHERE L.ISBN not in (select li.isbn from livre li ,livredepose lid where li.isbn=lid.isbn) AND (L.titre LIKE \"%$data%\" OR L.auteur LIKE \"%$data%\")";
+  $requeteLivreNotice = "SELECT distinct titre,auteur,image,L.isbn FROM livre L, LIVREDEPOSE LD WHERE L.ISBN=LD.ISBN AND (L.titre LIKE \"%$data%\" OR L.auteur LIKE \"%$data%\")";
+  $requeteLivreDepose = "SELECT distinct titre,auteur, image,L.isbn FROM livre L, LIVREDEPOSE LD WHERE L.ISBN not in (select li.isbn from livre li ,livredepose lid where li.isbn=lid.isbn) AND (L.titre LIKE \"%$data%\" OR L.auteur LIKE \"%$data%\")";
   $livres = $db->query($requeteLivreNotice); //Récupération des informations
   if ($livres) {
     $result["livres"] = array();
     foreach ($livres as $livreBD) {
       $livre = array();
+      $livre["isbn"] = $livreBD["ISBN"];
       $livre["titre"] = $livreBD["TITRE"];
       $livre["auteur"] = $livreBD["AUTEUR"];
       $livre["image"] = $livreBD["IMAGE"];
@@ -24,6 +25,7 @@ if(isset($data)){
     $result["livresdep"] = array();
     foreach ($livresdep as $livreBD) {
       $livredep = array();
+      $livredep["isbn"] = $livreBD["ISBN"];
       $livredep["titre"] = $livreBD["TITRE"];
       $livredep["auteur"] = $livreBD["AUTEUR"];
       $livredep["image"] = $livreBD["IMAGE"];
