a2ba72eae879abd2003a7814f949154213794ac2
SELMIRE Thibaud <selmiret@hostname.iut2.upmf-grenoble.fr>
2016-10-19 15:15:19 +0200

    wallah

--- js/compte.js
+++ js/compte.js
@@ -1,5 +1,21 @@
 function load_compte()
 {
   $("#content").empty();
-  $("#content").load("pages/compte.html");
+  $("#content").load("pages/compte.html",on_load());
+  on_load_js();
+}
+function on_load(){
+  var temp = "Tête de bite";
+  $("#niketamere").val();
+  var x = document.getElementById("content");
+  $("#niketamere").val(temp);
+  $("#newemail").attr('value',temp);
+  $("#newville").val("MA GROSSE VILLE");
+
+
+}
+function on_load_js()
+{
+  document.getElementById("niketamere").value("WALLAH");
+
 }
