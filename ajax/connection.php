449143a2c21bced74c4ffa7eb364e8c63a36007e
Scorsiro <tom.munier@gmail.com>
2016-10-19 10:27:06 +0200

    commit

--- ajax/connection.php
+++ ajax/connection.php
@@ -5,12 +5,18 @@ $result = array() ;               //Création du tableau à envoyer
 $result["status"] = "success";    //Initialisation du premier élément avec "sucess"
 
   // Vérification des identifiants
-  $pseudo = $_POST["login"];
-  $pass = $_POST["mdp"];
+  //$pseudo = $_POST["login"];
+  //$pass = $_POST["mdp"];
+  $pseudo = $argv[1];
+  $pass = $argv[2];
   $requete = "SELECT * FROM user WHERE login=\"$pseudo\";"; //Préparation de la requête
   $resultat = $db->query($requete); //Récupération des informations
   $tempRes = $resultat->fetch();
+  var_dump($tempRes);
+  $pass = $pass+'';
+  $tempRes["MDP"] = $tempRes["MDP"]+'';
   if(strcmp($pass,$tempRes["MDP"]) == 0){
+  //if ($pass == $tempRes["MDP"]){
     $result["log"] = "true";
     $result["login"] = $pseudo;
     $result["id"] = $tempRes["ID"];
