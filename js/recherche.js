1290c9a362ca8f2c5e70400db9c961605c156a4f
Bypus <stockerj@hostname.iut2.upmf-grenoble.fr>
2016-10-19 13:57:47 +0200

    Clear

--- js/recherche.js
+++ js/recherche.js
@@ -41,29 +41,34 @@ function afficherListeSouhait(){
 
 function displayResultContent(data){
   var toPrint = '<h4> Autres livres existants </h4> <ul class="thumbnails">';
-  for (var i=0; i < data.livresdep.length; i++) {
-    //structure
-    toPrint += '<li class="span3">';
-    toPrint += '<div class="thumbnail">';
-    //?
-    toPrint += "<a href=\"product.html?id="+data.livresdep[i].titre+"\">";
-    // titre
-    toPrint += "<h5>"+data.livresdep[i].titre+"</h5>";
-    // auteur
-    toPrint += "<p>"+data.livresdep[i].auteur+"</p>";
-    // l'image
-    toPrint += "<img src=\""+data.livresdep[i].image+"\" alt=\"\"/></a>";
-
-    toPrint += '<div class="caption">';
-
-    toPrint += '<h4 style="text-align:center"><a class="btn" href="product.html?id='+data.livresdep[i].titre+'">';
-    toPrint += '<i class="icon-zoom-in"></i></a> <a class="btn" href="#">Ajouter <i class="icon-shopping-cart"></i></a>';
-    if(data.livresdep[i].val != null){
-      toPrint += '<a class="btn btn-primary" href="#">'+data.livresdep[i].val+'</a></h4>';
-    } else {
-      toPrint +='</h4>';
+  if (data.livresdep.length == 0) {
+    toPrint += "<p>Pas de livre correspondant à votre recherche</p>";
   }
-  toPrint += "</div></div></li>";
+  else {
+    for (var i=0; i < data.livresdep.length; i++) {
+      //structure
+      toPrint += '<li class="span3">';
+      toPrint += '<div class="thumbnail">';
+      //?
+      toPrint += "<a href=\"product.html?id="+data.livresdep[i].titre+"\">";
+      // titre
+      toPrint += "<h5>"+data.livresdep[i].titre+"</h5>";
+      // auteur
+      toPrint += "<p>"+data.livresdep[i].auteur+"</p>";
+      // l'image
+      toPrint += "<img src=\""+data.livresdep[i].image+"\" alt=\"\"/></a>";
+
+      toPrint += '<div class="caption">';
+
+      toPrint += '<h4 style="text-align:center"><a class="btn" href="product.html?id='+data.livresdep[i].titre+'">';
+      toPrint += '<i class="icon-zoom-in"></i></a> <a class="btn" href="#">Ajouter <i class="icon-shopping-cart"></i></a>';
+      if(data.livresdep[i].val != null){
+        toPrint += '<a class="btn btn-primary" href="#">'+data.livresdep[i].val+'</a></h4>';
+      } else {
+        toPrint +='</h4>';
+      }
+    toPrint += "</div></div></li>";
+    }
   }
   $("#content").append(toPrint);
 }
@@ -71,35 +76,40 @@ function displayResultContent(data){
 
 function displayResultDB(data){
 
-var toPrint = '<h4> Livres disponibles </h4> <ul class="thumbnails">';
-  for (var i=0; i < data.livres.length; i++) {
-    //structure
-    toPrint += '<li class="span3">';
-    toPrint += '<div class="thumbnail">';
-    //?
-    toPrint += "<a href=\"product.html?id="+data.livres[i].titre+"\">";
-    // titre
-    toPrint += "<h5>"+data.livres[i].titre+"</h5>";
-    // auteur
-    toPrint += "<p>"+data.livres[i].auteur+"</p>";
-    // l'image
-    toPrint += "<img src=\""+data.livres[i].image+"\" alt=\"\"/></a>";
-
-    toPrint += '<div class="caption">';
-
-    toPrint += '<h4 style="text-align:center"><a class="btn" href="product.html?id='+data.livres[i].titre+'">';
-    toPrint += '<i class="icon-zoom-in"></i></a> <a class="btn" href="#">Ajouter <i class="icon-shopping-cart"></i></a>';
-    if(data.livres[i].val != null){
-      toPrint += '<a class="btn btn-primary" href="#">'+data.livres[i].val+'</a></h4>';
-    } else {
-      toPrint +='</h4>';
+  var toPrint = '<h4> Livres disponibles </h4> <ul class="thumbnails">';
+  if (data.livresdep.length == 0) {
+    toPrint += "<p>Pas de livre disponible correspondant à votre recherche</p>";
   }
-  toPrint += "</div></div></li>";
-  }
+  else {
+    for (var i=0; i < data.livres.length; i++) {
+      //structure
+      toPrint += '<li class="span3">';
+      toPrint += '<div class="thumbnail">';
+      //?
+      toPrint += "<a href=\"product.html?id="+data.livres[i].titre+"\">";
+      // titre
+      toPrint += "<h5>"+data.livres[i].titre+"</h5>";
+      // auteur
+      toPrint += "<p>"+data.livres[i].auteur+"</p>";
+      // l'image
+      toPrint += "<img src=\""+data.livres[i].image+"\" alt=\"\"/></a>";
+
+      toPrint += '<div class="caption">';
+
+      toPrint += '<h4 style="text-align:center"><a class="btn" href="product.html?id='+data.livres[i].titre+'">';
+      toPrint += '<i class="icon-zoom-in"></i></a> <a class="btn" href="#">Ajouter <i class="icon-shopping-cart"></i></a>';
+      if(data.livres[i].val != null){
+        toPrint += '<a class="btn btn-primary" href="#">'+data.livres[i].val+'</a></h4>';
+      } else {
+        toPrint +='</h4>';
+      }
+      toPrint += "</div></div></li>";
+      }
+    }
   $("#content").append(toPrint);
 }
 
-function displayResultContent(data){
+function displayListeSouhait(data){
   var toPrint = '<h4> Vos livres souhaités </h4> <ul class="thumbnails">';
   for (var i=0; i < data.livres.length; i++) {
     //structure
